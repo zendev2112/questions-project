dee8a543d60393ec32121f874c4c0639057d23e1
zen <zenon.deviagge@gmail.com>
2024-03-11 18:38:30 -0300

    add activeId and toggleQuestion props, remove previous state value

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,17 @@ import Questions from './Questions';
 import data from './data'
 import { useState } from 'react'
 const App = () => {
-  const [questions, setQuestions] = useState(data)
+  const [questions, setQuestions] = useState(data);
+  const [activeId, setActiveId] = useState(null) ;
+
+  const toggleQuestion = (id) => {
+    console.log(id);
+  }
   return <main>
-    <Questions questions={questions}/>
+    <Questions 
+    questions={questions} 
+    activeId={activeId} 
+    toggleQuestion={toggleQuestion}/>
   </main>;
 };
 export default App;
